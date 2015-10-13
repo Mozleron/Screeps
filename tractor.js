f90f409acbd1104893fe86db6095fe26ce4f8023
Mozleron <mozleron@gmail.com>
2015-10-12 21:36:24 -0700

    changing quotes

--- tractor.js
+++ tractor.js
@@ -57,7 +57,7 @@
                         else
                         {
                             creep.memory.action = "unload";
-                            var c = creep.room.find(FIND_MY_CREEPS, {filter: {role: 'truck'}});
+                            var c = creep.room.find(FIND_MY_CREEPS, {filter: {role: "truck"}});
                             console.log("c: "+c);
                             var cc = creep.pos.findClosestByRange(c);
                             console.log("cc: "+cc);
